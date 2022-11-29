4f9d6cc7bda7931331bb76c66592cdb75441d41c
David Krtolica <david.krtolica8@gmail.com>
2022-11-29 17:03:24 +0100

    Fixed seedArtists data - predefined IDs to match the Artwork

--- seed/artistSeedData.js
+++ seed/artistSeedData.js
@@ -1,5 +1,6 @@
 const seedArtists = [
     {
+        id: 'd14bb9fc-65dc-11ed-967a-931420ba6ae2', 
         fullName: 'Agostino Carracci',
         citedName: 'Carracci, Agostino',
         nationality: 'Italian',
@@ -8,10 +9,12 @@ const seedArtists = [
         deathDate: '2007-06-07'    
     },
     {
+        id: 'd14bc19a-65dc-11ed-967a-ab9e22dda3a3',
         fullName: 'Maribel Mamborini',
         citedName: 'Mambo Maribel'
     },
     {
+        id: 'd14bc230-65dc-11ed-967a-6ffdda8dcde8',
         fullName: 'Tim Mossholder',
         citedName: 'Mossholder',
         nationality: 'American',
@@ -19,10 +22,12 @@ const seedArtists = [
         birthPlace: 'Providence, Rhode Island'
     },
     {
+        id: 'd14bc2a8-65dc-11ed-967a-9300e6f1c4fe',
         fullName: 'Sansibar Markedes',
         citedName: 'S.O.C.I.A.L.'
     },
     {
+        id: 'd14bc316-65dc-11ed-967a-13759b11a418',
         fullName: 'Camille Coubertaine',
         nationality: 'French',
     }
